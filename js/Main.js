550240ca4e92279dce41a6de522dc30e8fe2fd6c
cris2265 <cehernandez@scl.edu.gt>
2024-06-02 23:03:00 -0600

    Diseño caluladora

---
+++ js/Main.js
@@ -0,0 +1,24 @@
+let root = document.querySelector("#root")
+
+    root.innerHTML = `
+        <input type="text" placeholder="0">
+        <button class="gris">AC</button>
+        <button class="gris">+/-</button>
+        <button class="gris">%</button>
+        <button class="naranja">÷</button>
+        <button>7</button>
+        <button>8</button>
+        <button>9</button>
+        <button class="naranja">X</button>
+        <button>4</button>
+        <button>5</button>
+        <button>6</button>
+        <button class="naranja">-</button>
+        <button>1</button>
+        <button>2</button>
+        <button>3</button>
+        <button class="naranja">+</button>
+        <button class="doble">0</button>
+        <button>,</button>
+        <button class="naranja">=</button>
+    `
